4a380136b85d0abb6de283ac9af11fe003f372c5
zhenyus <sunnyszy@gmail.com>
2019-01-13 09:05:35 -0500

    fix: write result per contract

--- concolic_execution/batch_run.py
+++ concolic_execution/batch_run.py
@@ -39,22 +39,21 @@ if len(sys.argv) >= 3:
     print "Job %d: Running on %d contracts..." % (job, len(contracts))
 cntnum = 0
 start_time = time.time()
-with open('results_' + str(job) + '.json', 'w') as of_result:
-    for c in contracts:
-        # c = '0x00c06521148cf463d4b51552d86237918243e9b4'
-        print "contract: " + c
-        with open('tmp_' + str(job) + '.evm', 'w') as of:
-            # print "Out: "+cjson[c][1][2:]
-            of.write(cjson[c][1][2:] + "\0")
-        # exit(0)
-        os.system('python2 oyente.py tmp_' + str(job) + '.evm -j -b')
-        try:
-            results[c] = json.loads(open('tmp_' + str(job) + '.evm.json').read())
-        except:
-            results[c] = {}
-        # if cntnum == 2:
+for c in contracts:
+    # c = '0x00c06521148cf463d4b51552d86237918243e9b4'
+    print "contract: " + c
+    with open('tmp_' + c + '.evm', 'w') as of:
+        # print "Out: "+cjson[c][1][2:]
+        of.write(cjson[c][1][2:] + "\0")
+    # exit(0)
+    os.system('python2 oyente.py tmp_' + c + '.evm -j -b')
+    try:
+        results[c] = json.loads(open('tmp_' + c + '.evm.json').read())
+    except:
+        results[c] = {}
+    # if cntnum == 2:
+    with open('results_' + c + '.json', 'w') as of_result:
         of_result.write(json.dumps(results, indent=1))
-        of_result.flush()
         # break
         cntnum += 1
 print "Completed."
